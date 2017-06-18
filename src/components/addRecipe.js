3d5f75474ae8be17bc009530a17b81c077aec32a
Edwin Moses <workockmoses@gmail.com>
2017-06-19 00:45:40 +0300

    form control

--- src/components/addRecipe.js
+++ src/components/addRecipe.js
@@ -3,8 +3,17 @@ import React from 'react';
 class AddRecipe extends React.Component {
   render() {
     return(
-      <div>
-        
+      <div className="card">
+        <form>
+          <div className="form-group">
+            <label className="form-control-label">Menu Title</label>
+            <input className="form-control" ref="menu" type="text" />
+          </div>
+          <div className="form-group">
+            <label className="form-control-label">Ingredients</label>
+            <textarea className="form-control" ref="ingredients"></textarea>
+          </div>
+        </form>
       </div>
     );
   }
