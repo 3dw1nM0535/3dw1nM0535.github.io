e7ebd0bf00302b77f1da21c48d788d7dc0ce7b4c
Edwin Moses <workockmoses@gmail.com>
2017-06-18 22:18:23 +0300

    recipe render item

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import RecipeItem from './recipeItem';
 
 class Recipe extends React.Component {
   render() {
-    console.log(this.props.recipes);
+    let recipeItem;
+    if (this.props.recipes) {
+      recipeItem = this.props.recipes.map((item, i) => {
+        return (
+          <RecipeItem item={item} key={i} />
+        );
+      })
+    }
     return(
-      <div>
-
+      <div className="container">
+        <div className="jumbotron">
+          {recipeItem}
+        </div>
       </div>
     );
   }
