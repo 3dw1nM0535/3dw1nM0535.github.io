0278673423c5ae29e49ca56ad53d26fd2024db5d
Edwin Moses <workockmoses@gmail.com>
2017-06-18 19:32:15 +0300

    home component display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Home from './components/Home';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <Home />        
       </div>
     );
   }
