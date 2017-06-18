7b3bc7a1787817381ad6d0cb45088d3d5b5ed608
Edwin Moses <workockmoses@gmail.com>
2017-06-19 00:49:28 +0300

    form block card

--- src/components/addRecipe.js
+++ src/components/addRecipe.js
@@ -4,7 +4,9 @@ class AddRecipe extends React.Component {
   render() {
     return(
       <div className="card">
-        <form>
+        <h3 className="card-title">Add Recipe</h3>
+        <div className="card-block">
+          <form>
           <div className="form-group">
             <label className="form-control-label">Menu Title</label>
             <input className="form-control" ref="menu" type="text" />
@@ -14,6 +16,7 @@ class AddRecipe extends React.Component {
             <textarea className="form-control" ref="ingredients"></textarea>
           </div>
         </form>
+        </div>
       </div>
     );
   }
