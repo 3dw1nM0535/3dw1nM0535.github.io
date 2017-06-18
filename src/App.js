3f84588020cab2243e6df119df613b8f4d93ac30
Edwin Moses <workockmoses@gmail.com>
2017-06-18 20:12:01 +0300

    App state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import Home from './components/Home';
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      recipes
+    }
+  }
+
+  componentWillMount() {
+    this.setState({recipes: [
+      {
+        menu: "Kachumbari",
+        ingredients: ["Onions", "Tomatoes", "Pepper"]
+      },
+      {
+        menu: "Pilau",
+        ingredients: ["Rice grains", "Pilau", "Binziari", "Onions"]
+      },
+      {
+        menu: "Ugali Stew",
+        ingredients: ["Unga flour", "Kales", "Red meat", "Tomatoes", "Onions"]
+      },
+      {
+        menu: "Pooding",
+        ingredients: ["Avocado", "Pawpaw", "Pineapple", "Apple", "Passion fruit"]
+      }
+    ]})
+  }
+
   render() {
     return (
       <div>
