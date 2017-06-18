df506c3ee75ef57678c785bb12a80ac60464de74
Edwin Moses <workockmoses@gmail.com>
2017-06-18 23:35:41 +0300

    Footer delete

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="container text-center">
-        <div className="jumbotron">
+        <div className="cover jumbotron">
           <h1 className="display-2">Recipe Box Menu</h1>
         </div>
       </div>
