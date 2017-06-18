7f319a6c660314d7fcc8207f7e45d977f9efbc62
Edwin Moses <workockmoses@gmail.com>
2017-06-18 20:04:32 +0300

    components

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../src/App.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container text-center">
+        <div className="jumbotron">
+          <h1 className="display-2">Recipe Box Menu</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
