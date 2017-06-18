31ae0174a064e89f7250016131a5ddf6718442b7
Edwin Moses <workockmoses@gmail.com>
2017-06-18 23:18:18 +0300

    uuid module to recipe object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import uuid from 'uuid';
 import Home from './components/Home';
 import Recipe from './components/Recipe';
 import './App.css';
@@ -14,18 +15,22 @@ class App extends React.Component {
   componentWillMount() {
     this.setState({recipes: [
       {
+        id: uuid.v4(),
         menu: "Kachumbari",
         ingredients: ["Onions", "Tomatoes", "Pepper"]
       },
       {
+        id: uuid.v4(),
         menu: "Pilau",
         ingredients: ["Rice grains", "Pilau", "Binziari", "Onions"]
       },
       {
+        id: uuid.v4(),
         menu: "Ugali Stew",
-        ingredients: ["Unga flour", "Kales", "Red meat", "Tomatoes", "Onions"]
+        ingredients: ["Maize flour", "Kales", "Red meat", "Tomatoes", "Onions"]
       },
       {
+        id: uuid.v4(),
         menu: "Pooding",
         ingredients: ["Avocado", "Pawpaw", "Pineapple", "Apple", "Passion fruit"]
       }
