da43e98154618cdd0a704c556de9206bebbffe50
Edwin Moses <workockmoses@gmail.com>
2017-06-18 21:14:00 +0300

    recipe item UI component

---
+++ src/components/recipeItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class RecipeItem extends React.Component {
+  render() {
+    return(
+      <div className='card'>
+        <h3 className="card-title">{}</h3>
+        <div className='card-block'>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RecipeItem;
