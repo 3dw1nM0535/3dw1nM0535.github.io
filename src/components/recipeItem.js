a07476c5206927a4bf4ddc1a579cf0164cce3154
Edwin Moses <workockmoses@gmail.com>
2017-06-18 22:34:33 +0300

    item render component

--- src/components/recipeItem.js
+++ src/components/recipeItem.js
@@ -4,7 +4,7 @@ class RecipeItem extends React.Component {
   render() {
     return(
       <div className='card spacing'>
-        <h3 className="card-header">{this.props.item.title}</h3>
+        <h3 className="card-header">{this.props.item.menu}</h3>
         <div className='card-block'>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
