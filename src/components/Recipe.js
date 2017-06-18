0c68abbed92bdee77c38cc0ef3d7c484628c95e3
Edwin Moses <workockmoses@gmail.com>
2017-06-18 21:13:15 +0300

    logic for recipe

---
+++ src/components/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Recipe extends React.Component {
+  render() {
+    console.log(this.props.recipes);
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Recipe;
