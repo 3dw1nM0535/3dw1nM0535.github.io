3150852fa92a165a62cb44f1204d6fdca344d80c
Edwin Moses <workockmoses@gmail.com>
2017-06-19 13:25:07 +0300

    add recipe form

--- src/components/addRecipe.js
+++ src/components/addRecipe.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
 class AddRecipe extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      newRecipe: {}
+    }
+  }
+
+  handleSubmit(e) {
+    if (this.refs.menu.value === "" && this.refs.ingredients.value === "") {
+      alert("Menu title and Ingredients can't be Empty!");
+    } else {
+      this.setState({newRecipe: {
+        menu: this.refs.menu.value,
+        ingredients: this.refs.ingredients.value
+      }}, function() {
+        this.props.addRecipe(this.state.newRecipe);
+      });
+    }
+    e.preventDefault();
+  }
+
   render() {
     return(
       <div className="card">
         <h3 className="card-title">Add Recipe</h3>
         <div className="card-block">
-          <form>
+          <form onSubmit={this.handleSubmit.bind(this)}>
           <div className="form-group">
             <label className="form-control-label">Menu Title</label>
             <input className="form-control" ref="menu" type="text" />
@@ -15,6 +36,9 @@ class AddRecipe extends React.Component {
             <label className="form-control-label">Ingredients</label>
             <textarea className="form-control" ref="ingredients"></textarea>
           </div>
+          <di>
+            <input type="submit" className="btn btn-outline-secondary btn-sm" value="Submit" />
+          </di>
         </form>
         </div>
       </div>
