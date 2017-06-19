c6954a4f9092df68a7f6221fae2d4af6c469c3b4
Edwin Moses <workockmoses@gmail.com>
2017-06-19 19:23:07 +0300

    delete edit function

--- src/components/recipeItem.js
+++ src/components/recipeItem.js
@@ -5,31 +5,6 @@ class RecipeItem extends React.Component {
     this.props.onDelete(id);
   }
 
-  editRecipe(id) {
-    return (
-      <div className="container">
-        <div className="card" style={{width: "30rem"}}>
-          <h3 className="card-title text-center">Add Recipe</h3>
-          <div className="card-block">
-            <form>
-            <div className="form-group">
-              <label className="form-control-label">Menu Title</label>
-              <input className="form-control" ref="menu" type="text" />
-            </div>
-            <div className="form-group">
-              <label className="form-control-label">Ingredients</label>
-              <textarea className="form-control" ref="ingredients"></textarea>
-            </div>
-            <div>
-              <input type="submit" className="btn btn-outline-primary btn-sm" value="Submit" />
-            </div>
-          </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   render() {
     return(
       <div className='card spacing'>
@@ -46,7 +21,7 @@ class RecipeItem extends React.Component {
           </ul>
         </div>
         <div>
-          <button type="button" onClick={this.editRecipe.bind(this)} className="btn btn-outline-primary btn-spacing">Edit</button>
+          <button type="button" className="btn btn-outline-primary btn-spacing">Edit</button>
           <button type="button" onClick={this.deleteRecipe.bind(this, this.props.item.id)} className="btn btn-outline-danger btn-spacing">Delete</button>
         </div>
       </div>
