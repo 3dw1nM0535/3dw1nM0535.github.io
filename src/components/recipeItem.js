6dcaf0aad3b61723569a49c82bdfb834ba41d9a3
Edwin Moses <workockmoses@gmail.com>
2017-06-21 11:58:44 +0300

    onSubmit on edit

--- src/components/recipeItem.js
+++ src/components/recipeItem.js
@@ -17,8 +17,9 @@ class RecipeItem extends React.Component {
     this.setState({editing: true});
   }
 
-  saveEdit() {
+  saveEdit(e) {
     this.setState({editing: false});
+    e.preventDefault();
   }
 
   renderNormal() {
@@ -50,14 +51,14 @@ class RecipeItem extends React.Component {
       <div className="card">
         <h4 className="card-title text-center">Edit Recipe</h4>
         <div className="card-block">
-          <form>
+          <form onSubmit={this.saveEdit.bind(this)}>
           <div className="form-group">
             <input className="form-control" ref="editedTitle" defaultValue={this.props.item.menu} type="text" style={{width: "20rem"}} />
           </div>
           <div className="form-group">
             <textarea className="form-control" ref="editedIngredients" defaultValue={this.props.item.ingredients} style={{width: "25rem"}}></textarea>
           </div>
-          <button type="button" className="btn btn-success" onClick={this.saveEdit.bind(this)}>Save</button>
+          <input type="submit" className="btn btn-success" value="Save" />
         </form>
       </div>
      </div>
