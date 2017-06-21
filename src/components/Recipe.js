d59f9a354da34f216e7bf7f04657d5cfcd18f221
Edwin Moses <workockmoses@gmail.com>
2017-06-21 03:02:00 +0300

    edit props

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,20 +3,19 @@ import '../../src/App.css';
 import RecipeItem from './recipeItem';
 
 class Recipe extends React.Component {
-  deleteRecipe(id) {
-    this.props.onDelete(id);
+  deleteRecipe(i) {
+    this.props.onDelete(i);
   }
 
-  edit(newRecipe, id) {
-    this.props.updateRecipe(newRecipe, id);
+  edit() {
+    this.props.updateRecipe();
   }
-
   render() {
     let recipeItem;
     if (this.props.recipes) {
       recipeItem = this.props.recipes.map((item, i) => {
         return (
-          <RecipeItem updateRecipe={this.edit.bind(this)} onDelete={this.deleteRecipe.bind(this)} item={item} key={i} />
+          <RecipeItem updateRecipe={this.edit.bind(this)} onDelete={this.deleteRecipe.bind(this)} recipes={this.props.recipes} item={item} key={i} />
         );
       })
     }
